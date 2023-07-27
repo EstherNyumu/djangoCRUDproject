ab969b39fc238d0b67a89f98f799c241cfd00c7f
EstherNyumu <emutheu1998@gmail.com>
2023-07-27 08:36:54 +0300

    Another commit

--- morningDjangoHtmlFormsProject/settings.py
+++ morningDjangoHtmlFormsProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jmp%5e2t82#6bc2(5x8l&vls^zo+edge#m86!=!%3(lsgnhsd!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'morningDjangoHtmlFormsProject',
     'crispy_forms',
-    'crispy_bootstrap4'
+    'crispy_bootstrap4',
+    'django_daraja'
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
